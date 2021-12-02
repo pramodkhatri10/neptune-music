7600d76af8f30bca2acf08e1f445d2c1e223e87c
Ruja Rajbhandari <rrajbhandari@mail.sfsu.edu>
2021-12-02 14:54:37 -0800

    rujarajbhandari Instrument added + visualizer color

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -8,8 +8,10 @@ import { WaveformVisualizer } from './visualizers/Waveform';
 
 import { RingVisualizer } from "./visualizers/Pramodkhatri10";
 import { GuitarInstrument } from "./instruments/Pramodkhatri10";
-
 import { FluteInstrument } from "./instruments/seelapant13";
+import { XylophoneInstrument } from "./instruments/rujarajbhandari";
+
+
 import { WallCurtainVisualizer } from './visualizers/salonimahat';
 import { CrossSquareVisualizer } from "./visualizers/rujarajbhandari";
 
@@ -29,7 +31,8 @@ export type AppState = Map<string, any>;
 
 const instruments = List([PianoInstrument,
   GuitarInstrument, //Pramodkhatri10
-  FluteInstrument //seelapant13
+  FluteInstrument, //seelapant13
+  XylophoneInstrument //rujarajbhandari
 ]);
 const visualizers = List([WaveformVisualizer,
   RingVisualizer,  //Pramodkhatri10
--- client/src/instruments/rujarajbhandari.tsx
+++ client/src/instruments/rujarajbhandari.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import { useEffect } from "react";
+import { Synth } from "tone";
+import { Instrument, InstrumentProps } from "../Instruments";
 
-const rujarajbhandari = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+
+const notes = ["C6", "D6", "E6", "F6", "G6", "A6", "B6", "C7", "D7", "E7"];
+
+export function Xylophone(props: InstrumentProps) {
+  const { synth, setSynth } = props;
+
+  useEffect(() => {
+    setSynth((oldSynth) => {
+      oldSynth.disconnect();
+
+      return new Synth({
+        oscillator: {
+          type: "fatsquare1",
+        },
+        envelope: {
+          attack: 0.005,
+          decay: 0.2,
+          sustain: 0.4,
+          release: 1.4,
+        },
+      }).toDestination();
+    });
+  }, [setSynth]);
+
+  return (
+    <div className="xylophone">
+      {notes.map((note) => (
+        <div
+          className="xylophone-note"
+          onMouseDown={() => synth?.triggerAttack(`${note}`)}
+          onMouseUp={() => synth?.triggerRelease("+0.25")}
+        >
+          {note}
+        </div> 
+      ))}
+    </div>
+  );
 }
 
-export default rujarajbhandari
+export const XylophoneInstrument = new Instrument("rujarajbhandari", Xylophone); 
